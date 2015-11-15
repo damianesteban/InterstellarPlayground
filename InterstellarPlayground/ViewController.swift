95cd70d2a8e355c1a98a3520362bf087e21f93d9
Damian Esteban <damian.esteban@gmail.com>
2015-11-15 12:30:36 -0500

    Initial Commit

---
+++ InterstellarPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InterstellarPlayground
+//
+//  Created by Damian Esteban on 11/15/15.
+//  Copyright © 2015 Damian Esteban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
